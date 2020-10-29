072ebdd6312d46cd3aae32f287fbf3bd0e7ed6d5
Joong-Won Seo <joong-won.seo@tum.de>
2020-10-29 12:28:43 +0100

    fixed typo

--- src/pgdp/intro/Hallo.java
+++ src/pgdp/intro/Hallo.java
@@ -1,10 +1,10 @@
 package pgdp.intro;
 
-import static pgdp.MiniJava.*; //Dieses Import nicht löschen! Do not delete this import!
+import static pgdp.MiniJava.*; //Diesen Import nicht löschen! Do not delete this import!
 
 public class Hallo {
     public static void main(String[] args) {
         //TODO: Aufgabe bearbeiten...
-        
+
     }
 }
